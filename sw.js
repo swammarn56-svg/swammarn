97b465e5880aff386b224b68cbc9ccae37581716
swammarn56-svg <swammarn56@gmail.com>
2026-08-13 01:12:14 +0630

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bakery-erp-v14-phase2-browserfix-2";
+const CACHE="bakery-erp-v14-phase2-staticfix-1";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./css/style.css","./js/app.js","./js/utils.js","./js/supabase.js","./js/config.js"]))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
